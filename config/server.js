b3e9ae407e34fc816143d9374e47c8a1101bf35e
Marius Rusu <ionutm.rusu@hogarthww.com>
2024-09-25 09:36:50 +0300

    env

--- config/server.js
+++ config/server.js
@@ -1,10 +1,22 @@
 module.exports = ({ env }) => ({
-  host: env('HOST', '0.0.0.0'),
-  port: env.int('PORT', 1337),
-  app: {
-    keys: env.array('APP_KEYS'),
-  },
-  webhooks: {
-    populateRelations: env.bool('WEBHOOKS_POPULATE_RELATIONS', false),
+  defaultConnection: 'default',
+  connections: {
+    default: {
+      connector: 'bookshelf',
+      settings: {
+        client: 'postgres',  // PostgreSQL as the client
+        host: env('DATABASE_HOST'),  // Neon PostgreSQL host
+        port: env.int('DATABASE_PORT', 5432),  // PostgreSQL port
+        database: env('DATABASE_NAME'),  // Neon database name
+        username: env('DATABASE_USERNAME'),  // Neon username
+        password: env('DATABASE_PASSWORD'),  // Neon password
+        ssl: {
+          rejectUnauthorized: env.bool('DATABASE_SSL', true),  // Neon requires SSL
+        },
+      },
+      options: {
+        ssl: env.bool('DATABASE_SSL', true),
+      },
+    },
   },
 });
